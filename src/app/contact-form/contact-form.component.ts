759aad10c991ca6ba77c5375556516876ddb4a2c
Leandro De Jesus Jorge <ljorge@brq.com>
2018-11-17 18:42:01 -0200

    upload

---
+++ src/app/contact-form/contact-form.component.ts
@@ -0,0 +1,16 @@
+import { AppComponent } from './../app.component';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrls: ['./contact-form.component.css']
+})
+export class ContactFormComponent  {
+
+  
+  log(firstName) {
+    console.log(firstName)
+  }
+
+}
--- src/app/like/like.component.ts
+++ src/app/like/like.component.ts
@@ -37,4 +37,4 @@ export class LikeComponent implements OnInit {
    }
 
 }
-}
+
