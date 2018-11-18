3b1ebe5214dc86b9bec406709fb42016989325f4
Leandro De Jesus Jorge <ljorge@brq.com>
2018-11-18 13:46:48 -0200

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 
-
-
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
 
+import { CoursesComponent } from './courses.component';
 import { CoursesService } from './courses.Service';
 import { SummaryPipe } from './summary.pipe';
 import { FavoriteComponent } from './favorite/favorite.component';
@@ -17,6 +15,7 @@ import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 
 
@@ -25,20 +24,21 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
   declarations: [
     AppComponent,
     CoursesComponent,
-    
     SummaryPipe,
     FavoriteComponent,
     PanelComponent,
     LikeComponent,
     InputFormatDirective,
     ZippyComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    SignupFormComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [CoursesService],
   bootstrap: [AppComponent]
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent  {
+  contactMethods = [
+    {id: 1, name:'email'},
+    {id: 2, name:'carta'},
+    {id: 3, name:'SMS'},
+    {id: 4, name:'post'},
+    {id: 5, name:'teste'},
+]
 
+radios = [
+  {id: 1, name:'teste-1'},
+  {id: 2, name:'teste-2'},
+  {id: 3, name:'teste-3'},
+  {id: 4, name:'teste-4'},
+  {id: 5, name:'teste-5'},
+]
   
   log(firstName) {
     console.log(firstName)
+  }
+
+  submit(f){
+    console.log(f)
   }
 
 }
