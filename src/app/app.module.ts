884963197afd016e08cb28ce222883f06b90b19a
Leandro De Jesus Jorge <ljorge@brq.com>
2018-11-18 23:25:49 -0200

    upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
 
 
 
@@ -33,7 +34,8 @@ import { NewCourseFormComponent } from './new-course-form/new-course-form.compon
     ZippyComponent,
     ContactFormComponent,
     SignupFormComponent,
-    NewCourseFormComponent
+    NewCourseFormComponent,
+    ChangePasswordComponent
     
   ],
   imports: [
--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,15 +1,46 @@
+import { FormGroup, FormArray, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-new-course-form',
+  selector: 'new-course-form',
   templateUrl: './new-course-form.component.html',
   styleUrls: ['./new-course-form.component.css']
 })
-export class NewCourseFormComponent implements OnInit {
+export class NewCourseFormComponent {
+/*  form = new FormGroup({
+      name: new FormControl('',Validators.required),
+      contact: new FormControl({
+        email : new FormControl(),
+        phone : new FormControl()
+      }),
+      topics: new FormArray([])
+ }); 
+  duas maneiras de declaração as duas fazm a mesma coisa*/
+ form;
 
-  constructor() { }
+ constructor(fb: FormBuilder){
+  this.form = fb.group({
+     name: ['', Validators.required],
+     contact : fb.group({
+       email: [],
+       phone: [],
+     }),
+     topics: fb.array([])
+   });
+ }
 
-  ngOnInit() {
-  }
+ addTopic(topic : HTMLInputElement) {
+  this.topics.push(new FormControl(topic.value))
+    topic.value ='';
+ }
+
+ removeTopic(topic : FormControl) {
+  let index = this.topics.controls.indexOf(topic);
+  this.topics.removeAt(index);
+ }
+
+ get topics() {
+   return this.form.get('topics') as FormArray;
+ }
 
 }
